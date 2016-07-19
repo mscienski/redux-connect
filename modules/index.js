d013f33d1d4453580a9b7e09cea89ecd7777501b
Michal Scienski <mscienski@overstock.com>
2016-07-19 16:36:59 -0600

    Export action types from module

--- modules/index.js
+++ modules/index.js
@@ -1,4 +1,12 @@
 export ReduxAsyncConnect from './containers/AsyncConnect';
 export { asyncConnect } from './containers/decorator';
 export { loadOnServer } from './helpers/utils';
-export { reducer } from './store';
+export {
+    reducer,
+    LOAD,
+    LOAD_SUCCESS,
+    LOAD_FAIL,
+    CLEAR,
+    BEGIN_GLOBAL_LOAD,
+    END_GLOBAL_LOAD
+} from './store';
